327852ca0d0ebbefb417b43ba8ccb6a37c1c821e
eduardodgdebem <bemeduardode@gmail.com>
2022-06-12 21:10:01 -0300

    add not found page

---
+++ src/pages/notFound/index.jsx
@@ -0,0 +1,29 @@
+import { Button, Card } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import "./notFound.css";
+
+const NotFound = () => {
+  const navigate = useNavigate();
+
+  const redirectToLogin = () => {
+    navigate("/");
+  };
+  
+  return (
+    <main>
+      <Card className="notfound-card">
+        <h1>Página não encontrada!</h1>
+        <Button
+          variant="contained"
+          color="primary"
+          className="gradient-hover"
+          onClick={redirectToLogin}
+        >
+          Ir para a página de login
+        </Button>
+      </Card>
+    </main>
+  );
+};
+
+export default NotFound;
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/login";
 import NewsLetterPage from "./pages/newsletter";
+import NotFound from "./pages/notFound";
 import Register from "./pages/register";
 
 function RoutesApp() {
@@ -11,6 +12,7 @@ function RoutesApp() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/newsletter" element={<NewsLetterPage/>}></Route>
+        <Route path="*" element={<NotFound/>}></Route>
       </Routes>
     </BrowserRouter>
   );
