4d34e7f017faa3236f9240ade8832ff5e58ef47b
Adair Locks Grassi <adairlocksgrassi@gmail.com>
2022-06-12 21:57:11 -0300

    correção link

--- src/pages/notFound/index.jsx
+++ src/pages/notFound/index.jsx
@@ -1,26 +1,13 @@
 import { Button, Card } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./notFound.css";
 
 const NotFound = () => {
-  const navigate = useNavigate();
-
-  const redirectToLogin = () => {
-    navigate("/");
-  };
-
   return (
     <main className="notfound-main">
       <Card className="notfound-card">
         <h1>Página não encontrada!</h1>
-        <Button
-          variant="contained"
-          color="primary"
-          className="gradient-hover"
-          onClick={redirectToLogin}
-        >
-          Ir para a página de login
-        </Button>
+        <Link to='/login'>Ir para a página de login</Link>
       </Card>
     </main>
   );
